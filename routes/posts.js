b40407452d3efea0b54f826f2f34cd850a78ba2d
ttop5 <ttop5@qq.com>
2017-11-26 16:17:55 +0800

    feat(posts): 主页与文章页

--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,15 @@ const checkLogin = require('../middlewares/check').checkLogin
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
 router.get('/', function (req, res, next) {
-  res.render('posts')
+  const author = req.query.author
+
+  PostModel.getPosts(author)
+    .then(function(posts) {
+      res.render('posts', {
+        posts: posts
+      })
+    })
+    .catch(next)
 })
 
 // POST /posts/create 发表一篇文章
@@ -37,11 +45,11 @@ router.post('/create', checkLogin, function (req, res, next) {
   }
 
   PostModel.create(post)
-    .then(function(result) {
+    .then(function (result) {
       // 此 post 是插入 mongodb 后的值，包含 _id
       post = result.ops[0]
       req.flash('success', '发表成功')
-      // 发表成功后跳转到改文章页
+      // 发表成功后跳转到该文章页
       res.redirect(`/posts/${post._id}`)
     })
     .catch(next)
@@ -54,7 +62,23 @@ router.get('/create', checkLogin, function (req, res, next) {
 
 // GET /posts/:postId 单独一篇的文章页
 router.get('/:postId', function (req, res, next) {
-  res.send('文章详情页')
+  const postId = req.params.postId
+
+  Promise.all([
+    PostModel.getPostById(postId),  // 获取文章信息
+    PostModel.incPv(postId)  // pv +1
+  ])
+    .then(function(result) {
+      const post = result[0]
+      if (!post) {
+        throw new Error('改文章不存在')
+      }
+
+      res.render('post', {
+        post: post
+      })
+    })
+    .catch(next)
 })
 
 // GET /posts/:postId/edit 更新文章页
