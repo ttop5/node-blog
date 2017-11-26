36109f05adda943632e0354d2eb07c1dc50de3a4
ttop5 <ttop5@qq.com>
2017-11-26 11:12:26 +0800

    feat(signup): 注册

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ app.use(session({
 // flash 中间件，用来显示通知
 app.use(flash())
 
+// 处理表单及文件上传中间件
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'),  // 文件上传目录
+  keepExtensions: true  // 保留后缀
+}))
+
 // 设置全局模板常量
 app.locals.blog = {
   title: pkg.name,
